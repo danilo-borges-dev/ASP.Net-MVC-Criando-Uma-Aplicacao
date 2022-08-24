c398f6e461819a891875e53e7ff2d9d14379699a
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-08-24 14:07:39 -0300

    Praticando a criacao de sites com Razor

---
+++ MinhaAplicacaoMVC/MinhaAplicacaoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinhaAplicacaoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
