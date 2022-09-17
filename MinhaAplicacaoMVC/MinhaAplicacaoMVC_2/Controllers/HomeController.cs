98be85cc1ca772c9cd19668b3bdfb8f6b09ed8c0
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-17 11:45:29 -0300

    Praticando a criacao de Rotas e retornos

--- MinhaAplicacaoMVC/MinhaAplicacaoMVC_2/Controllers/HomeController.cs
+++ MinhaAplicacaoMVC/MinhaAplicacaoMVC_2/Controllers/HomeController.cs
@@ -6,14 +6,7 @@ namespace MinhaAplicacaoMVC_2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public IActionResult Index(string id, string categoria)
         {
             return View();
         }
---
+++ MinhaAplicacaoMVC/MinhaAplicacaoMVC_3/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaAplicacaoMVC_3.Models;
+using System.Diagnostics;
+
+namespace MinhaAplicacaoMVC_3.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes/")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("pagina-inicial/{id:int}/{categoria?}")]
+        public IActionResult Index(string id, Guid categoria)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("policita-de-privacidade")]
+        public IActionResult Privacy()
+        {
+            // return Json("{'nome':'Danilo'}");
+            var fileBytes = System.IO.File.ReadAllBytes(@"c:\arquivo.txt");
+            var fileName = "novo_arquivo.txt";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
